a00bae556faad68df327fd7d4f48d2555cfb8d85
giapnd04 <ndgiap2004@gmail.com>
2025-03-24 01:55:31 +0700

    Homeage update
    Basic homepage front-end

---
+++ frontend/CharityDonationFrontend/src/app/components/donation-list/donation-list.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DonationCardComponent } from '../donation-card/donation-card.component';
+
+@Component({
+  selector: 'app-donation-list',
+  standalone: true,
+  imports: [CommonModule, DonationCardComponent],
+  templateUrl: './donation-list.component.html',
+  styleUrls: ['./donation-list.component.css']
+})
+export class DonationListComponent {
+  donations = [
+    { title: "Hỗ trợ trẻ em nghèo", amount: 50000000 },
+    { title: "Cứu trợ lũ lụt miền Trung", amount: 100000000 },
+    { title: "Giúp đỡ bệnh nhân ung thư", amount: 70000000 }
+  ];
+}
