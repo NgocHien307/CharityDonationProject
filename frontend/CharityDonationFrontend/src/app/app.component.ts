307b2b62f49d24552fa983cba69f91690b66fbdf
giapnd04 <ndgiap2004@gmail.com>
2025-03-24 16:47:14 +0700

    Update front-end homepage
    Add partner and custom customize the homepage

--- frontend/CharityDonationFrontend/src/app/app.component.ts
+++ frontend/CharityDonationFrontend/src/app/app.component.ts
@@ -8,6 +8,7 @@ import { FooterComponent } from './components/footer/footer.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { StatisticsComponent } from './components/statistics/statistics.component';
 import { DonationListComponent } from './components/donation-list/donation-list.component';
+import { PartnerComponent } from "./components/partner/partner.component";
 
 @Component({
   selector: 'app-root',
@@ -18,8 +19,9 @@ import { DonationListComponent } from './components/donation-list/donation-list.
     FooterComponent,
     BannerComponent,
     StatisticsComponent,
-    DonationListComponent
-  ],
+    DonationListComponent,
+    PartnerComponent
+],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
