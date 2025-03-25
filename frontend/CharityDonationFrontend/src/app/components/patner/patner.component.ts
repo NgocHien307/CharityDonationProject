29c234ea37075b4b5fdfadfc834babaf2d89c20c
VuDuyTri <tri25012003@gmail.com>
2025-03-25 09:48:31 +0700

    fix conflict

---
+++ frontend/CharityDonationFrontend/src/app/components/patner/patner.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-patner',
+  imports: [],
+  templateUrl: './patner.component.html',
+  styleUrl: './patner.component.css'
+})
+export class PatnerComponent {
+
+}
