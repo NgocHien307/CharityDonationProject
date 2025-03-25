dc838e68470e89a74ce255787341de3fb65aeb81
VuDuyTri <tri25012003@gmail.com>
2025-03-25 11:14:30 +0700

    fixed home

---
+++ frontend/CharityDonationFrontend/src/app/components/home/home.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { BannerComponent } from '../banner/banner.component';
+import { StatisticsComponent } from '../statistics/statistics.component';
+import { DonationListComponent } from '../donation-list/donation-list.component';
+import { DonationCardComponent } from '../donation-card/donation-card.component';
+import { PartnerComponent } from '../partner/partner.component';
+import { FooterComponent } from '../footer/footer.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [HeaderComponent, BannerComponent, StatisticsComponent, DonationListComponent, DonationCardComponent, PartnerComponent, FooterComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
